c39e5a9b09b666c332f5713fdeccfccce746549a
Adam Kowalski <adam.kowalski.work@gmail.com>
2022-04-27 23:13:48 -0700

    mat4x4

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,141 @@
+import { ECS } from './ecs'
+import { projection, Mat4x4 } from './linear_algebra'
 
+class Geometry {
+  constructor(
+    public vertices: number[],
+    public indices: number[]
+  ) { }
+}
+
+class Translate {
+  constructor(
+    public x: number,
+    public y: number,
+    public z: number,
+  ) { }
+
+  matrix = (): Mat4x4 =>
+    new Mat4x4([
+      1, 0, 0, 0,
+      0, 1, 0, 0,
+      0, 0, 1, 0,
+      this.x, this.y, this.z, 1,
+    ])
+}
+
+class Scale {
+  constructor(
+    public x: number,
+    public y: number,
+    public z: number,
+  ) { }
+
+  matrix = (): Mat4x4 =>
+    new Mat4x4([
+      this.x, 0, 0, 0,
+      0, this.y, 0, 0,
+      0, 0, this.z, 0,
+      0, 0, 0, 1,
+    ])
+}
+
+const ecs = new ECS()
+
+const rect = ecs.entity(
+  new Geometry(
+    [
+      -0.5, 0.5, 0,
+      0.5, 0.5, 0,
+      -0.5, -0.5, 0,
+      0.5, -0.5, 0
+    ],
+    [
+      0, 1, 2,
+      2, 3, 1
+    ]
+  ),
+  new Translate(400, 500, 0),
+  new Scale(50, 50, 1)
+)
+
+
+const vertexShaderSource = `#version 300 es
+in vec4 a_position;
+
+uniform mat4 u_matrix;
+
+void main() {
+  gl_Position = u_matrix * a_position;
+}
+`
+
+const fragmentShaderSource = `#version 300 es
+precision mediump float;
+
+out vec4 fragColor;
+
+void main() {
+  fragColor = vec4(0.0, 0.0, 0.0, 1.0);
+}
+`
+
+const gl_canvas: HTMLCanvasElement = document.createElement('canvas')
+gl_canvas.style.width = '100%'
+gl_canvas.style.height = '100%'
+gl_canvas.style.position = 'absolute'
+document.body.appendChild(gl_canvas)
+const gl = gl_canvas.getContext('webgl2')
+
+gl.clearColor(1.0, 1.0, 1.0, 1.0)
+
+const vertexShader = gl.createShader(gl.VERTEX_SHADER)
+gl.shaderSource(vertexShader, vertexShaderSource)
+gl.compileShader(vertexShader)
+
+const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER)
+gl.shaderSource(fragmentShader, fragmentShaderSource)
+gl.compileShader(fragmentShader)
+
+const program = gl.createProgram()
+gl.attachShader(program, vertexShader)
+gl.attachShader(program, fragmentShader)
+gl.linkProgram(program)
+
+if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+  console.log(gl.getShaderInfoLog(vertexShader))
+  console.log(gl.getShaderInfoLog(fragmentShader))
+}
+gl.useProgram(program)
+
+const dpr = window.devicePixelRatio
+gl.canvas.width = Math.round(gl.canvas.clientWidth * dpr)
+gl.canvas.height = Math.round(gl.canvas.clientHeight * dpr)
+gl.viewport(0, 0, gl.canvas.width, gl.canvas.height)
+
+const uMatrix = gl.getUniformLocation(program, 'u_matrix')
+
+const positionBuffer = gl.createBuffer()
+const aPositionLocation = gl.getAttribLocation(program, 'a_position')
+gl.enableVertexAttribArray(aPositionLocation)
+
+const indexBuffer = gl.createBuffer()
+
+gl.clear(gl.COLOR_BUFFER_BIT)
+
+const geometry = rect.get(Geometry)
+gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer)
+gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(geometry.vertices), gl.STATIC_DRAW)
+gl.vertexAttribPointer(aPositionLocation, /*size*/3, /*type*/gl.FLOAT, /*normalize*/false, /*stride*/0, /*offset*/0)
+gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer)
+gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint8Array(geometry.indices), gl.STATIC_DRAW)
+
+const s = rect.get(Scale)
+
+const matrix = projection(gl.canvas.clientWidth, gl.canvas.clientHeight, 400)
+  .mul(rect.get(Translate).matrix())
+  .mul(rect.get(Scale).matrix())
+
+gl.uniformMatrix4fv(uMatrix, /*transpose*/false, matrix.data)
+
+gl.drawElements(gl.TRIANGLES, /*count*/6, /*index type*/gl.UNSIGNED_BYTE, /*offset*/0)
---
+++ src/linear_algebra.ts
@@ -0,0 +1,68 @@
+export class Mat4x4 {
+  constructor(public data: number[]) { }
+
+  mul = (other: Mat4x4): Mat4x4 => {
+    const a = this.data
+    const b = other.data
+    return new Mat4x4([
+      b[0] * a[0] + b[1] * a[4] + b[2] * a[8] + b[3] * a[12],
+      b[0] * a[1] + b[1] * a[5] + b[2] * a[9] + b[3] * a[13],
+      b[0] * a[2] + b[1] * a[6] + b[2] * a[10] + b[3] * a[14],
+      b[0] * a[3] + b[1] * a[7] + b[2] * a[11] + b[3] * a[15],
+      b[4] * a[0] + b[5] * a[4] + b[6] * a[8] + b[7] * a[12],
+      b[4] * a[1] + b[5] * a[5] + b[6] * a[9] + b[7] * a[13],
+      b[4] * a[2] + b[5] * a[6] + b[6] * a[10] + b[7] * a[14],
+      b[4] * a[3] + b[5] * a[7] + b[6] * a[11] + b[7] * a[15],
+      b[8] * a[0] + b[9] * a[4] + b[10] * a[8] + b[11] * a[12],
+      b[8] * a[1] + b[9] * a[5] + b[10] * a[9] + b[11] * a[13],
+      b[8] * a[2] + b[9] * a[6] + b[10] * a[10] + b[11] * a[14],
+      b[8] * a[3] + b[9] * a[7] + b[10] * a[11] + b[11] * a[15],
+      b[12] * a[0] + b[13] * a[4] + b[14] * a[8] + b[15] * a[12],
+      b[12] * a[1] + b[13] * a[5] + b[14] * a[9] + b[15] * a[13],
+      b[12] * a[2] + b[13] * a[6] + b[14] * a[10] + b[15] * a[14],
+      b[12] * a[3] + b[13] * a[7] + b[14] * a[11] + b[15] * a[15],
+    ])
+  }
+}
+
+export const xRotate = (radians: number): Mat4x4 => {
+  const c = Math.cos(radians)
+  const s = Math.sin(radians)
+  return new Mat4x4([
+    1, 0, 0, 0,
+    0, c, s, 0,
+    0, -s, c, 0,
+    0, 0, 0, 1,
+  ])
+}
+
+export const yRotate = (radians: number): Mat4x4 => {
+  const c = Math.cos(radians)
+  const s = Math.sin(radians)
+  return new Mat4x4([
+    c, 0, -s, 0,
+    0, 1, 0, 0,
+    s, 0, c, 0,
+    0, 0, 0, 1,
+  ])
+}
+
+export const zRotate = (radians: number): Mat4x4 => {
+  const c = Math.cos(radians)
+  const s = Math.sin(radians)
+  return new Mat4x4([
+    c, s, 0, 0,
+    -s, c, 0, 0,
+    0, 0, 1, 0,
+    0, 0, 0, 1,
+  ])
+}
+
+
+export const projection = (width: number, height: number, depth: number): Mat4x4 =>
+  new Mat4x4([
+    2 / width, 0, 0, 0,
+    0, -2 / height, 0, 0,
+    0, 0, 2 / depth, 0,
+    -1, 1, 0, 1,
+  ])

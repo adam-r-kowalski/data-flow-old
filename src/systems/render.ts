fca53753c2e46c95f90a912ee3e2fbebfd22154c
Adam Kowalski <adam.kowalski.work@gmail.com>
2022-05-17 18:12:58 -0700

    compute size before rendering

--- src/systems/render.ts
+++ src/systems/render.ts
@@ -3,9 +3,9 @@ import * as components from '../components'
 import { computeSize } from './compute_size'
 
 export const render = (ecs: ECS): void => {
+  computeSize(ecs)
   const renderer = ecs.get(components.Renderer)!
   renderer.clear()
-  computeSize(ecs)
   for (const layer of ecs.get(components.Layers)!.stack) {
     for (const entity of layer) {
       const color = entity.get(components.BackgroundColor)

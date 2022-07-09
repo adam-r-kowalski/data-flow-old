3ade1fe94ceef8dbe5e8018032e3dc005c99a17c
Adam Kowalski <adam.kowalski.work@gmail.com>
2022-07-09 16:22:44 -0700

    container within container geometry

--- test/ui/container.test.ts
+++ test/ui/container.test.ts
@@ -1,4 +1,4 @@
-import { container, layout, geometry } from '../../src/new_ui'
+import { container, layout, geometry, UIKind } from '../../src/new_ui'
 import { reduce } from '../../src/new_ui/reduce'
 import { initCameraStack } from '../../src/new_ui/camera_stack'
 import { mockDocument, mockWindow } from '../../src/renderer/mock'
@@ -13,6 +13,22 @@ const mockRenderer = () => webGL2Renderer({
     height: 500,
     document: mockDocument(),
     window: mockWindow()
+})
+
+test("container with width height and color", () => {
+    const ui = container({
+        width: 50,
+        height: 50,
+        color: red
+    })
+    const expectedUi = {
+        kind: UIKind.CONTAINER,
+        padding: { top: 0, right: 0, bottom: 0, left: 0 },
+        width: 50,
+        height: 50,
+        color: red
+    }
+    expect(ui).toEqual(expectedUi)
 })
 
 test("container layout", () => {
@@ -146,4 +162,70 @@ test("container batches", () => {
         }
     ]
     expect(batches).toEqual(expectedBatches)
+})
+
+test("container within container layout", () => {
+    const renderer = mockRenderer()
+    const ui = container({ padding: 5 },
+        container({
+            width: 50,
+            height: 50,
+            color: red
+        }))
+    const constraints = { minWidth: 0, maxWidth: 100, minHeight: 0, maxHeight: 100 }
+    const uiLayout = layout(ui, constraints, renderer.measureText)
+    const expectedLayout = {
+        size: { width: 60, height: 60 },
+        child: {
+            size: { width: 50, height: 50 }
+        }
+    }
+    expect(uiLayout).toEqual(expectedLayout)
+})
+
+test("container within container geometry", () => {
+    const renderer = mockRenderer()
+    const ui = container({ padding: 5 },
+        container({
+            width: 50,
+            height: 50,
+            color: red
+        }))
+    const constraints = { minWidth: 0, maxWidth: 100, minHeight: 0, maxHeight: 100 }
+    const uiLayout = layout(ui, constraints, renderer.measureText)
+    const offsets = { x: 0, y: 0 }
+    const cameraStack = initCameraStack()
+    const uiGeometry = geometry(ui, uiLayout, offsets, cameraStack)
+    const expectedGeometry = {
+        worldSpace: { x0: 0, y0: 0, x1: 60, y1: 60 },
+        vertices: [],
+        colors: [],
+        vertexIndices: [],
+        cameraIndex: [],
+        textureIndex: 0,
+        textureCoordinates: [],
+        child: {
+            worldSpace: { x0: 5, y0: 5, x1: 55, y1: 55 },
+            vertices: [
+                5, 5,
+                5, 55,
+                55, 5,
+                55, 55,
+            ],
+            colors: [
+                255, 0, 0, 255,
+                255, 0, 0, 255,
+                255, 0, 0, 255,
+                255, 0, 0, 255,
+            ],
+            vertexIndices: [
+                0, 1, 2,
+                1, 2, 3
+            ],
+            cameraIndex: Array(4).fill(0),
+            textureIndex: 0,
+            textureCoordinates: Array(8).fill(0),
+        }
+    }
+    expect(uiGeometry).toEqual(expectedGeometry)
 })

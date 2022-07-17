9c4885c2c1ce3d3d5fa870298f5459dafc58c9fb
Adam Kowalski <adam.kowalski.work@gmail.com>
2022-07-16 21:25:56 -0700

    as you zoom in the bezier curves still look great

--- src/ui/gather_connections.ts
+++ src/ui/gather_connections.ts
@@ -1,6 +1,13 @@
 import { Entry, Connection, UIKind } from "."
+import { inverse, multiplyMatrixVector } from "../linear_algebra/matrix3x3"
+import { length } from "../linear_algebra/vector3"
 
-export type Connections = Connection[][]
+export interface ConnectionsAndScale {
+    connections: Connection[]
+    scale: number
+}
+
+export type Connections = ConnectionsAndScale[][]
 
 export const initial = (): Connections => []
 
@@ -10,7 +17,10 @@ export const combine = <AppEvent>(connections: Connections, entry: Entry<AppEven
         const needed = entry.z - connections.length + 1
         for (let i = 0; i < needed; ++i) connections.push([])
         const layer = connections[entry.z]
-        layer.push(...entry.ui.connections)
+        layer.push({
+            connections: entry.ui.connections,
+            scale: length(multiplyMatrixVector(inverse(entry.ui.camera), [0, 1, 0]))
+        })
         return connections
     }
     return connections

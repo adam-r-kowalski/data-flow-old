037ad851408fcdeecb7a10a0680328939ca6bb0d
Adam Kowalski <adam.kowalski.work@gmail.com>
2022-07-24 21:45:02 -0700

    refactoring towards immutability

--- src/event.ts
+++ src/event.ts
@@ -2,8 +2,10 @@ import { fuzzyFind } from "./fuzzy_find"
 import { multiplyMatrices, multiplyMatrixVector, scale, translate } from "./linear_algebra/matrix3x3"
 import { length } from "./linear_algebra/vector3"
 import { UpdateResult } from "./ui/run"
-import { Edge, GenerateUUID, Input, InputTargetKind, Output, State, UUID, VirtualKeyboardKind } from "./state"
+import { InputTargetKind, State, VirtualKeyboardKind } from "./state"
+import { Edge, GenerateUUID, Input, Output, UUID } from './graph/model'
 import { Pointer } from "./ui"
+import { addEdge, addNode, changeNodePosition } from "./graph/update"
 
 export enum EventKind {
     POINTER_MOVE,
@@ -111,252 +113,310 @@ export type AppEvent =
 
 const pointerDown = (state: State, event: PointerDown): UpdateResult<State, AppEvent> => {
     if (state.inputTarget.kind !== InputTargetKind.NONE) return { state }
-    state.pointers.push(event.pointer)
-    if (state.pointers.length > 1) {
-        state.potentialDoubleClick = false
-        state.dragging = false
-        state.zooming = state.pointers.length === 2
-        return { state }
-    }
-    if (state.potentialDoubleClick) {
-        state.potentialDoubleClick = false
+    const pointers = [...state.pointers, event.pointer]
+    if (pointers.length > 1) {
         return {
-            state,
+            state: {
+                ...state,
+                potentialDoubleClick: false,
+                dragging: false,
+                zooming: pointers.length === 2,
+                pointers
+            }
+        }
+    } else if (state.potentialDoubleClick) {
+        return {
+            state: {
+                ...state,
+                potentialDoubleClick: false,
+                pointers
+            },
             dispatch: [{ kind: EventKind.DOUBLE_CLICK, pointer: event.pointer }]
         }
-    }
-    state.dragging = true
-    state.potentialDoubleClick = true
-    return {
-        state,
-        schedule: [
-            { after: { milliseconds: 300 }, event: { kind: EventKind.DOUBLE_CLICK_TIMEOUT } }
-        ]
+    } else {
+        return {
+            state: {
+                ...state,
+                dragging: true,
+                potentialDoubleClick: true,
+                pointers
+            },
+            schedule: [
+                { after: { milliseconds: 300 }, event: { kind: EventKind.DOUBLE_CLICK_TIMEOUT } }
+            ]
+        }
     }
 }
 
-const pointerUp = (state: State, event: PointerUp) => {
-    const index = state.pointers.findIndex(p => p.id === event.pointer.id)
-    state.pointers.splice(index, 1)
-    if (state.pointers.length === 1) {
-        state.zooming = false
-        state.dragging = true
-        state.pointerDistance = 0
+const pointerUp = (state: State, event: PointerUp): UpdateResult<State, AppEvent> => {
+    const pointers = state.pointers.filter(p => p.id !== event.pointer.id)
+    switch (pointers.length) {
+        case 1: return {
+            state: {
+                ...state,
+                pointers,
+                zooming: false,
+                dragging: true,
+                pointerDistance: 0
+            }
+        }
+        case 0: return {
+            state: {
+                ...state,
+                pointers,
+                dragging: false,
+                pointerDistance: 0
+            }
+        }
+        default: return { state: { ...state, pointers } }
     }
-    else if (state.pointers.length === 0) {
-        state.dragging = false
-        state.pointerDistance = 0
-    }
-    return { state }
 }
 
-const pointerMove = (state: State, event: PointerMove) => {
+export const changeNth = <T>(xs: Readonly<T[]>, i: number, x: T): T[] =>
+    [...xs.slice(0, i), x, ...xs.slice(i)]
+
+const pointerMove = (state: State, event: PointerMove): UpdateResult<State, AppEvent> => {
     if (!state.dragging && !state.zooming) {
-        if (!state.finder.show) {
-            state.nodePlacementLocation = { x: event.pointer.x, y: event.pointer.y }
-        }
-        return { state, rerender: false }
-    }
-    const index = state.pointers.findIndex(p => p.id === event.pointer.id)
-    const pointer = state.pointers[index]
-    state.pointers[index] = event.pointer
-    if (state.dragging) {
-        const dx = event.pointer.x - pointer.x
-        const dy = event.pointer.y - pointer.y
-        if (state.selectedNode) {
-            const scaling = length(multiplyMatrixVector(state.camera, [0, 1, 0]))
-            const node = state.graph.nodes[state.selectedNode]
-            node.x += dx * scaling
-            node.y += dy * scaling
+        const nodePlacementLocation = !state.finder.show ?
+            event.pointer.position :
+            state.nodePlacementLocation
+        return { state: { ...state, nodePlacementLocation } }
+    } else {
+        const index = state.pointers.findIndex(p => p.id === event.pointer.id)
+        const pointer = state.pointers[index]
+        const pointers = changeNth(state.pointers, index, event.pointer)
+        if (state.dragging) {
+            const dx = event.pointer.position.x - pointer.position.x
+            const dy = event.pointer.position.y - pointer.position.y
+            if (state.selectedNode) {
+                const scaling = length(multiplyMatrixVector(state.camera, [0, 1, 0]))
+                const graph = changeNodePosition(state.graph, state.selectedNode, p => ({
+                    x: p.x + dx * scaling,
+                    y: p.y + dy * scaling,
+                }))
+                return {
+                    state: { ...state, pointers, graph },
+                    render: true
+                }
+            } else {
+                const camera = multiplyMatrices(state.camera, translate(-dx, -dy))
+                return {
+                    state: { ...state, pointers, camera },
+                    render: true
+                }
+            }
         } else {
-            state.camera = multiplyMatrices(state.camera, translate(-dx, -dy))
+            // must be zooming
+            const [p0, p1] = [state.pointers[0], state.pointers[1]]
+            const [x1, y1] = [p0.position.x, p0.position.y]
+            const [x2, y2] = [p1.position.x, p1.position.y]
+            const pointerDistance = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2))
+            const previousDistance = state.pointerDistance
+            const previousCenter = state.pointerCenter
+            const x = (p0.position.x + p1.position.x) / 2
+            const y = (p0.position.y + p1.position.y) / 2
+            const pointerCenter = { x, y }
+            if (previousDistance > 0) {
+                const move = translate(x, y)
+                const zoom = Math.pow(2, (previousDistance - pointerDistance) * 0.01)
+                const moveBack = translate(-x, -y)
+                const dx = x - previousCenter.x
+                const dy = y - previousCenter.y
+                const camera = multiplyMatrices(state.camera, move, scale(zoom, zoom), moveBack, translate(-dx, -dy))
+                return {
+                    state: { ...state, pointerCenter, pointerDistance, camera },
+                    render: true
+                }
+            } else {
+                return { state: { ...state, pointerCenter, pointerDistance } }
+            }
         }
-        return { state, render: true }
     }
-    // must be zooming
-    const [p0, p1] = [state.pointers[0], state.pointers[1]]
-    const [x1, y1] = [p0.x, p0.y]
-    const [x2, y2] = [p1.x, p1.y]
-    const distance = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2))
-    const previousDistance = state.pointerDistance
-    const previousCenter = state.pointerCenter
-    state.pointerDistance = distance
-    const x = (p0.x + p1.x) / 2
-    const y = (p0.y + p1.y) / 2
-    state.pointerCenter = [x, y]
-    if (previousDistance > 0) {
-        const move = translate(x, y)
-        const zoom = Math.pow(2, (previousDistance - distance) * 0.01)
-        const moveBack = translate(-x, -y)
-        const dx = x - previousCenter[0]
-        const dy = y - previousCenter[1]
-        state.camera = multiplyMatrices(state.camera, move, scale(zoom, zoom), moveBack, translate(-dx, -dy))
-        return { state, render: true }
-    } else return { state }
 }
 
-const clickedNode = (state: State, event: ClickedNode) => {
-    state.selectedNode = event.node
+const clickedNode = (state: State, event: ClickedNode): UpdateResult<State, AppEvent> => {
     const nodeOrder = state.nodeOrder.filter(uuid => uuid !== event.node)
     nodeOrder.push(event.node)
-    state.nodeOrder = nodeOrder
-    return { state, render: true }
+    return {
+        state: { ...state, selectedNode: event.node, nodeOrder },
+        render: true
+    }
 }
 
-const wheel = (state: State, event: Wheel) => {
+const wheel = (state: State, event: Wheel): UpdateResult<State, AppEvent> => {
     const move = translate(event.x, event.y)
     const zoom = Math.pow(2, event.deltaY * 0.01)
     const moveBack = translate(-event.x, -event.y)
-    state.camera = multiplyMatrices(state.camera, move, scale(zoom, zoom), moveBack)
-    return { state, render: true }
+    const camera = multiplyMatrices(state.camera, move, scale(zoom, zoom), moveBack)
+    return {
+        state: { ...state, camera },
+        render: true
+    }
 }
 
-const clickedInput = (state: State, event: ClickedInput, generateUUID: GenerateUUID) => {
+const clickedInput = (state: State, event: ClickedInput, generateUUID: GenerateUUID): UpdateResult<State, AppEvent> => {
     if (state.selectedOutput) {
-        const edge: Edge = {
-            uuid: generateUUID(),
+        const { graph } = addEdge({
+            graph: state.graph,
             input: event.input,
-            output: state.selectedOutput
+            output: state.selectedOutput,
+            generateUUID
+        })
+        return {
+            state: {
+                ...state,
+                selectedInput: undefined,
+                selectedOutput: undefined,
+                graph
+            },
+            render: true
         }
-        state.graph.edges[edge.uuid] = edge
-        state.selectedInput = undefined
-        state.selectedOutput = undefined
-        return { state, render: true }
+    } else {
+        return {
+            state: { ...state, selectedInput: event.input },
+            render: true
+        }
     }
-    state.selectedInput = event.input
-    return { state, render: true }
 }
 
-const clickedOutput = (state: State, event: ClickedOutput, generateUUID: GenerateUUID) => {
+const clickedOutput = (state: State, event: ClickedOutput, generateUUID: GenerateUUID): UpdateResult<State, AppEvent> => {
     if (state.selectedInput) {
-        const edge: Edge = {
-            uuid: generateUUID(),
+        const { graph } = addEdge({
+            graph: state.graph,
             input: state.selectedInput,
-            output: event.output
+            output: event.output,
+            generateUUID
+        })
+        return {
+            state: {
+                ...state,
+                selectedInput: undefined,
+                selectedOutput: undefined,
+                graph
+            },
+            render: true
         }
-        state.graph.edges[edge.uuid] = edge
-        state.selectedInput = undefined
-        state.selectedOutput = undefined
-        return { state, render: true }
+    } else {
+        return {
+            state: { ...state, selectedOutput: event.output },
+            render: true
+        }
     }
-    state.selectedOutput = event.output
-    return { state, render: true }
 }
 
-const doubleClickTimeout = (state: State, _: DoubleClickTimeout) => {
-    if (state.potentialDoubleClick) {
-        state.potentialDoubleClick = false
-    }
-    return { state }
-}
+const doubleClickTimeout = (state: State, _: DoubleClickTimeout): UpdateResult<State, AppEvent> => ({
+    state: { ...state, potentialDoubleClick: false }
+})
 
 const updateFinderOptions = (state: State): State => {
-    state.finder.options = Object.keys(state.operations)
+    const options = Object.keys(state.operations)
         .filter(item => fuzzyFind({ haystack: item, needle: state.finder.search }))
-    return state
+    return { ...state, finder: { ...state.finder, options } }
 }
 
-export const openFinder = (state: State) => {
-    state.finder.show = true
-    state.finder.search = ''
-    state.virtualKeyboard = {
-        show: true,
-        kind: VirtualKeyboardKind.ALPHABETIC
-    }
-    state.inputTarget = { kind: InputTargetKind.FINDER }
-    state.potentialDoubleClick = false
-    state = updateFinderOptions(state)
-    return state
-}
+export const openFinder = (state: State): State =>
+    updateFinderOptions({
+        ...state,
+        finder: {
+            show: true,
+            search: '',
+            options: []
+        },
+        virtualKeyboard: {
+            show: true,
+            kind: VirtualKeyboardKind.ALPHABETIC
+        },
+        inputTarget: { kind: InputTargetKind.FINDER },
+        potentialDoubleClick: false
+    })
 
 
-const doubleClick = (state: State, { pointer }: DoubleClick) => {
-    state = openFinder(state)
-    state.nodePlacementLocation = { x: pointer.x, y: pointer.y }
-    return { state: state, render: true }
-}
+const doubleClick = (state: State, { pointer }: DoubleClick): UpdateResult<State, AppEvent> => ({
+    state: openFinder({
+        ...state,
+        nodePlacementLocation: pointer.position
+    }),
+    render: true
+})
 
-const closeFinder = (state: State) => {
-    state.finder.show = false
-    state.finder.search = ''
-    state.virtualKeyboard = {
+const closeFinder = (state: State): State => ({
+    ...state,
+    finder: {
+        show: false,
+        search: '',
+        options: []
+    },
+    virtualKeyboard: {
         show: false,
         kind: VirtualKeyboardKind.ALPHABETIC
-    }
-    state.inputTarget = { kind: InputTargetKind.NONE }
-    return state
-}
+    },
+    inputTarget: { kind: InputTargetKind.NONE }
+})
 
 const insertOperationFromFinder = (state: State, name: string, generateUUID: GenerateUUID): State => {
-    state = closeFinder(state)
     const operation = state.operations[name]
     const [x, y, _] = multiplyMatrixVector(
         state.camera,
         [state.nodePlacementLocation.x, state.nodePlacementLocation.y, 1]
     )
-    const uuid = generateUUID()
-    const inputs = operation.inputs.map(name => {
-        const input: Input = {
-            uuid: generateUUID(),
-            name,
-        }
-        state.graph.inputs[input.uuid] = input
-        return input.uuid
+    const { graph, node } = addNode({
+        graph: state.graph,
+        operation,
+        position: { x, y },
+        generateUUID
     })
-    const outputs = operation.outputs.map(name => {
-        const output: Output = {
-            uuid: generateUUID(),
-            name,
-            edges: [],
-        }
-        state.graph.outputs[output.uuid] = output
-        return output.uuid
+    return closeFinder({
+        ...state,
+        graph,
+        nodeOrder: [...state.nodeOrder, node]
     })
-    state.graph.nodes[uuid] = {
-        uuid,
-        name,
-        inputs,
-        outputs,
-        x,
-        y,
-        body: operation.body !== undefined ? {
-            value: operation.body,
-            editing: false
-        } : undefined
-    }
-    state.nodeOrder.push(uuid)
-    return state
 }
 
-const keyDown = (state: State, { key }: KeyDown, generateUUID: GenerateUUID) => {
+const keyDown = (state: State, { key }: KeyDown, generateUUID: GenerateUUID): UpdateResult<State, AppEvent> => {
     switch (state.inputTarget.kind) {
         case InputTargetKind.FINDER:
             switch (key) {
                 case 'Backspace':
-                    state.finder.search = state.finder.search.slice(0, -1)
-                    break
+                    return {
+                        state: updateFinderOptions({
+                            ...state,
+                            finder: {
+                                ...state.finder,
+                                search: state.finder.search.slice(0, -1)
+                            }
+                        }),
+                        render: true
+                    }
                 case 'Shift':
                 case 'Alt':
                 case 'Control':
                 case 'Meta':
                 case 'Tab':
-                    break
+                    return { state }
                 case 'Enter':
                     if (state.finder.options.length > 0) {
                         const name = state.finder.options[0]
-                        state = insertOperationFromFinder(state, name, generateUUID)
+                        return {
+                            state: insertOperationFromFinder(state, name, generateUUID),
+                            render: true
+                        }
                     } else {
-                        state = closeFinder(state)
+                        return { state: closeFinder(state), render: true }
                     }
-                    break
                 case 'Escape':
-                    state = closeFinder(state)
-                    break
+                    return { state: closeFinder(state), render: true }
                 default:
-                    state.finder.search += key
-                    break
+                    return {
+                        state: updateFinderOptions({
+                            ...state,
+                            finder: {
+                                ...state.finder,
+                                search: state.finder.search + key
+                            }
+                        }),
+                        render: true
+                    }
             }
-            return { state: updateFinderOptions(state), render: true }
         case InputTargetKind.NUMBER:
             const node = state.graph.nodes[state.inputTarget.nodeUUID]
             let value = node.body!.value.toString()

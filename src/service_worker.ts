b17f5d7dab7df978001b6e86dbd3e28feac98439
Adam Kowalski <adam.kowalski.work@gmail.com>
2022-08-14 21:26:53 -0700

    installable!

---
+++ src/service_worker.ts
@@ -0,0 +1,25 @@
+export default null
+
+declare var self: ServiceWorkerGlobalScope
+
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches
+            .open('studio')
+            .then(cache => cache.addAll([]))
+    )
+})
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if (response) {
+                    return response
+                } else {
+                    return fetch(event.request)
+                }
+            })
+    )
+})
